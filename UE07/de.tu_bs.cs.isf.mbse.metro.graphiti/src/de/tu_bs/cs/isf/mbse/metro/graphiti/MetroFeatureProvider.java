fe1db55dd7fe907d6cf1a14c3f060514eeb9aee2
bellisoy <bellisoy@eren>
2016-01-25 19:06:53 +0100

    ewtte

---
+++ UE07/de.tu_bs.cs.isf.mbse.metro.graphiti/src/de/tu_bs/cs/isf/mbse/metro/graphiti/MetroFeatureProvider.java
@@ -0,0 +1,12 @@
+package de.tu_bs.cs.isf.mbse.metro.graphiti;
+
+import org.eclipse.graphiti.dt.IDiagramTypeProvider;
+import org.eclipse.graphiti.ui.features.DefaultFeatureProvider;
+
+public class MetroFeatureProvider extends DefaultFeatureProvider {
+
+	public MetroFeatureProvider(IDiagramTypeProvider dtp) {
+		super(dtp);
+	}
+
+}
